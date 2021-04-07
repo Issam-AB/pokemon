f92f7c01bf16714a6c446bc8b8b0b24f7d36e534
ABOULFADL <issam.aboulfadl05@gmail.com>
2021-04-07 16:47:07 +0100

    connect database with mongo atlas

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Pokemon Battle</title>
+				<link
+					href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+					rel="stylesheet"
+				></link>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
